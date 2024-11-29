e0b1ca6250b0eb3a355220d3165e215cd3b3382e
Pradipkumar Desai <pdesai.programming@gmail.com>
2024-11-29 22:28:29 +0530

    Initial commit

---
+++ src/test/java/com/pd/job_portal_api/JobPortalApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pd.job_portal_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobPortalApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
