9db2e691582b16b59303ec1967efa8bd5d7f3c7b
Pradipkumar Desai <pdesai.programming@gmail.com>
2024-11-30 16:15:42 +0530

    User REST API

--- src/main/java/com/pd/job_portal_api/user/controllers/UserController.java
+++ src/main/java/com/pd/job_portal_api/user/controllers/UserController.java
@@ -3,15 +3,15 @@ package com.pd.job_portal_api.user.controllers;
 import com.pd.job_portal_api.user.entities.User;
 import com.pd.job_portal_api.user.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
-@RequestMapping("users")
+@RestController
+@RequestMapping("api/users")
 public class UserController {
 
     @Autowired
