a0d1a0b4cf9291fc2bf8427fcdb46eae70e06f95
Pradipkumar Desai <pdesai.programming@gmail.com>
2024-12-02 20:22:21 +0530

    Docker and K8S support added with env var

--- feeds/src/main/java/com/pd/jobportal/feeds/configuration/WebConfig.java
+++ feeds/src/main/java/com/pd/jobportal/feeds/configuration/WebConfig.java
@@ -1,5 +1,6 @@
 package com.pd.jobportal.feeds.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,12 +8,24 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    @Value("${cors.allowed.origins}")
+    private String[] allowedOrigins;
+
+    @Value("${cors.allowed.methods}")
+    private String[] allowedMethods;
+
+    @Value("${cors.allowed.headers}")
+    private String[] allowedHeaders;
+
+    @Value("${cors.allow.credentials}")
+    private boolean allowCredentials;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Allow CORS for all endpoints
-                .allowedOrigins("http://localhost:3000") // Allow requests from React app running on localhost:3000
-                .allowedMethods("GET", "POST", "PUT", "DELETE") // Allow specific HTTP methods
-                .allowedHeaders("*") // Allow all headers
-                .allowCredentials(true); // Allow credentials (cookies, HTTP authentication)
+                .allowedOrigins(allowedOrigins) // Allow requests from React app running on localhost:3000
+                .allowedMethods(allowedMethods) // Allow specific HTTP methods
+                .allowedHeaders(allowedHeaders) // Allow all headers
+                .allowCredentials(allowCredentials); // Allow credentials (cookies, HTTP authentication)
     }
 }
--- src/main/java/com/pd/job_portal_api/configuration/WebConfig.java
+++ src/main/java/com/pd/job_portal_api/configuration/WebConfig.java
@@ -1,5 +1,6 @@
 package com.pd.job_portal_api.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -8,13 +9,25 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    @Value("${cors.allowed.origins}")
+    private String[] allowedOrigins;
+
+    @Value("${cors.allowed.methods}")
+    private String[] allowedMethods;
+
+    @Value("${cors.allowed.headers}")
+    private String[] allowedHeaders;
+
+    @Value("${cors.allow.credentials}")
+    private boolean allowCredentials;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Allow CORS for all endpoints
-                .allowedOrigins("http://localhost:3000") // Allow requests from React app running on localhost:3000
-                .allowedMethods("GET", "POST", "PUT", "DELETE") // Allow specific HTTP methods
-                .allowedHeaders("*") // Allow all headers
-                .allowCredentials(true); // Allow credentials (cookies, HTTP authentication)
+                .allowedOrigins(allowedOrigins) // Allow requests from React app running on localhost:3000
+                .allowedMethods(allowedMethods) // Allow specific HTTP methods
+                .allowedHeaders(allowedHeaders) // Allow all headers
+                .allowCredentials(allowCredentials); // Allow credentials (cookies, HTTP authentication)
     }
 }
 
